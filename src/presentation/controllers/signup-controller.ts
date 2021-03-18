5a379001e4a447f777a2c2c338698d312e7259a6
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-18 10:25:32 -0300

    feat: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,12 +1,17 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { AddAccount } from '@/domain/usecases'
+import { serverError } from '@/presentation/helpers'
 
 export class SignUpController implements Controller {
   constructor (private readonly addAccount: AddAccount) { }
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
-    const { email, password } = request
-    await this.addAccount.add({ email, password })
-    return null
+    try {
+      const { email, password } = request
+      await this.addAccount.add({ email, password })
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
