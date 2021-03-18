12166209b18c1f9e76de43f48060f2a56e84db16
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-18 14:23:14 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,11 +1,13 @@
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { AddAccount } from '@/domain/usecases'
-import { serverError } from '@/presentation/helpers'
+import { serverError, badRequest } from '@/presentation/helpers'
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: AddAccount) { }
+  constructor (private readonly addAccount: AddAccount, private readonly validation: Validation) { }
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(request)
+      if (error) return badRequest(error)
       const { email, password } = request
       await this.addAccount.add({ email, password })
       return null
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -5,3 +5,8 @@ export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
 })
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
