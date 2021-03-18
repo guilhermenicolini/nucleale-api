d7f89d502b28710465429edccaa211c8d8c93cbd
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-18 14:27:04 -0300

    test: ensure SignUpController returns 400 if Validation returns an error

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '@/presentation/controllers'
 import { AddAccountSpy, ValidationSpy } from '@/tests/presentation/mocks'
 import { throwError } from '@/tests/domain/mocks'
-import { serverError } from '@/presentation/helpers'
+import { serverError, badRequest } from '@/presentation/helpers'
 import { ServerError } from '@/presentation/errors'
 
 import faker from 'faker'
@@ -55,5 +55,12 @@ describe('SignUp Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 if Validation returns an error ', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
