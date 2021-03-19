d30dd01378b31906406cad0256c31e1394ffb968
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-19 13:40:56 -0300

    test: ensure api returns xml when forced

--- tests/main/middlewares/content-type.test.ts
+++ tests/main/middlewares/content-type.test.ts
@@ -5,10 +5,20 @@ import request from 'supertest'
 describe('Content Type Middleware', () => {
   test('Should return json as default content type', async () => {
     app.get('/test-content-type', (req, res) => {
-      res.send('')
+      res.send()
     })
     await request(app)
       .get('/test-content-type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
   })
 })
