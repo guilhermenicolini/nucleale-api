a1c41f168bc91df46967ded565c386ee333ef871
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-19 08:46:03 -0300

    chore: add @shelf/jest-mongodb

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '.+\\.ts$': 'ts-jest'
   },
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'
