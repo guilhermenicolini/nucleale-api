b57e97b65a5a7a809ff2ccf430912666c1e3f1b4
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-19 15:06:54 -0300

    feat: make DbAddAccount

---
+++ src/main/factories/usecases/add-account-factory.ts
@@ -0,0 +1,11 @@
+import { DbAddAccount } from '@/data/usecases'
+import { AddAccount } from '@/domain/usecases'
+import { AccountMongoRepository } from '@/infra/db'
+import { BCryptAdapter } from '@/infra/cryptography'
+
+export const makeDbAddAccount = (): AddAccount => {
+  const salt = 12
+  const bcryptAdapter = new BCryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository)
+}
