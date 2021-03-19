a81f17251d121af88f09e94681a670fd08530c79
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-19 14:29:45 -0300

    test: ensure EmailValidation returns an error if EmailValidator returns false

---
+++ tests/validation/validators/email-validation.spec.ts
@@ -0,0 +1,31 @@
+import { EmailValidation } from '@/validation/validators'
+import { InvalidParamError } from '@/presentation/errors'
+import { EmailValidatorSpy } from '@/tests/validation/mocks'
+
+import faker from 'faker'
+
+const field = faker.random.word()
+
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation(field, emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
+}
+
+describe('Email Validation', () => {
+  test('Should return an error if EmailValidator returns false', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.isEmailValid = false
+    const email = faker.internet.email()
+    const error = sut.validate({ [field]: email })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+})
