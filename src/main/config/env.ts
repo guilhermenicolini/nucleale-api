4ab2351350b1f9554db55393e384edc9e32f05c4
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-20 09:51:22 -0300

    chore: add dotenv only for dev

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,8 @@
 export default {
   mongoUrl: process.env.MONGO_URL,
-  port: process.env.PORT || 5050,
   secret: process.env.JWT_SECRET || 'secret',
+  port: process.env.PORT || 5050,
   iss: process.env.JWT_ISS || 'Issuer',
   exp: process.env.JWT_EXP || '1h',
-  aud: process.env.JWT_AUD || 'https://any.com'
+  aud: process.env.JWT_AUD || 'https://issuer.com'
 }
