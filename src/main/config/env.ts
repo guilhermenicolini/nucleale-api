ebc7984ba3f00d7247921d3b356dc70fb6751e38
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-19 22:26:42 -0300

    chore: remove env configurations

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,8 @@
 export default {
   mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050,
-  secret: process.env.JWT_SECRET || 'j6HZScwueTrDd78JYhSn2cJrGauF9csdMbgP6mVFp2YfvAnnqRWHKyns2LwNrJNjDJVBRkQVDauAWPrVur2QPNwjF22FnGGD9b4L5j7DvpWYYBTrqCpwBnYxGaD3WgRp',
-  iss: process.env.JWT_ISS || 'Nucleale',
+  secret: process.env.JWT_SECRET || 'secret',
+  iss: process.env.JWT_ISS || 'Issuer',
   exp: process.env.JWT_EXP || '1h',
-  aud: process.env.JWT_AUD || 'https://nucleale.com'
+  aud: process.env.JWT_AUD || 'https://any.com'
 }
