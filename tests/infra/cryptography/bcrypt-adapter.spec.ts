4b0132d394c6bb65ced478758c7ea9df0026fd88
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-03-24 00:46:47 -0300

    test: ensure BcryptAdapter calls compare with correct values

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -37,4 +37,13 @@ describe('BCrypt Adapter', () => {
       await expect(promise).rejects.toThrow()
     })
   })
+
+  describe('compare()', () => {
+    test('Should call compare with correct values', async () => {
+      const sut = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('any_value', 'any_hash')
+      expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    })
+  })
 })
