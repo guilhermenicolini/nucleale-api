cc7a907c887fbd93afeeb6e9c2cbd972ff65f7f6
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-10 18:29:39 -0300

    test: add ApproveAccountValidation

---
+++ tests/main/factories/approve-account-validation-factory.spec.ts
@@ -0,0 +1,18 @@
+import { makeApproveAccountValidation } from '@/main/factories'
+import {
+  ValidationComposite,
+  IdValidation
+} from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+import { IdValidatorAdapter } from '@/infra/validators'
+
+jest.mock('@/validation/validators/validation-composite')
+
+describe('ApproveAccountValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeApproveAccountValidation()
+    const validations: Validation[] = []
+    validations.push(new IdValidation('id', new IdValidatorAdapter()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
