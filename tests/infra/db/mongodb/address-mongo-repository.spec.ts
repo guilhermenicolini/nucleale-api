46c4e9aa188c52b333ed86bb4b91c04d54a51bd4
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-12 23:04:35 -0300

    test: ensure AddressMongoRepository save address on success with correct values

---
+++ tests/infra/db/mongodb/address-mongo-repository.spec.ts
@@ -0,0 +1,46 @@
+import { AddressMongoRepository, MongoHelper } from '@/infra/db'
+import { mockAddressModel } from '@/tests/domain/mocks'
+
+import { Collection } from 'mongodb'
+
+const makeSut = (): AddressMongoRepository => {
+  return new AddressMongoRepository()
+}
+
+let addressesCollection: Collection
+
+describe('AddressMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.instance.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.instance.disconnect()
+  })
+
+  beforeEach(async () => {
+    addressesCollection = await MongoHelper.instance.getCollection('addresses')
+    await addressesCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('Should save on success', async () => {
+      const sut = makeSut()
+      const data = mockAddressModel()
+      await sut.save(data)
+      const addresses = await addressesCollection.find({}).toArray()
+      expect(addresses.length).toBe(1)
+      expect(addresses[0]._id).toBeTruthy()
+      expect(addresses[0].accountId.toString()).toBe(data.accountId)
+      expect(addresses[0].address).toBe(data.address)
+      expect(addresses[0].number).toBe(data.number)
+      expect(addresses[0].complement).toBe(data.complement)
+      expect(addresses[0].district).toBe(data.district)
+      expect(addresses[0].city).toBe(data.city)
+      expect(addresses[0].cityId).toBe(data.cityId)
+      expect(addresses[0].state).toBe(data.state)
+      expect(addresses[0].country).toBe(data.country)
+      expect(addresses[0].zip).toBe(data.zip)
+    })
+  })
+})
