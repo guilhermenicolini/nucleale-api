8e479ad8417351568a86daf34cca8ad78850ae2e
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-12 23:38:23 -0300

    refactor: organize factories and usecases

---
+++ src/main/factories/usecases/account/index.ts
@@ -0,0 +1,7 @@
+export * from './add-account-factory'
+export * from './invite-account-factory'
+export * from './load-account-factory'
+export * from './load-accounts-by-status-factory'
+export * from './load-invitation-factory'
+export * from './save-account-factory'
+export * from './verify-account-factory'
