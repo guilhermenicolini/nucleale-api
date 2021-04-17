d7ffe5b4a5477cb0d281f9b86e553fc6438ff623
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-17 18:05:32 -0300

    test: ensure AddChildrenController returns 400 if Validation returns an error

--- tests/presentation/controllers/children/add-children-controller.spec.ts
+++ tests/presentation/controllers/children/add-children-controller.spec.ts
@@ -1,6 +1,7 @@
 import { AddChildrenController } from '@/presentation/controllers'
 import { ValidationSpy, AddChildrenSpy } from '@/tests/presentation/mocks'
 import { mockAddChildrenModel } from '@/tests/domain/mocks'
+import { badRequest } from '@/presentation/helpers'
 
 type SutTypes = {
   sut: AddChildrenController,
@@ -25,5 +26,12 @@ describe('AddChildren Controller', () => {
     const request = mockAddChildrenModel()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 if Validation returns an error ', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(mockAddChildrenModel())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
