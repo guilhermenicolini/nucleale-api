3ec8fb2ad8647d882f809b2e43dad3b2253e4af7
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-17 19:05:10 -0300

    test: ensure NumberValidation not returns if validation succeeds

--- tests/validation/validators/number-validation.spec.ts
+++ tests/validation/validators/number-validation.spec.ts
@@ -15,4 +15,10 @@ describe('NumberValidation', () => {
     const error = sut.validate({ invalidField: faker.random.word() })
     expect(error).toEqual(new InvalidParamError(field))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ [field]: faker.random.number() })
+    expect(error).toBeFalsy()
+  })
 })
