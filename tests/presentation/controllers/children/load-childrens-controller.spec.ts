45e1901e5bd244f9bf3b81f77f6e337d8dbbab1a
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-18 18:37:40 -0300

    test: ensure LoadChildrensController returns 200 on success

--- tests/presentation/controllers/children/load-childrens-controller.spec.ts
+++ tests/presentation/controllers/children/load-childrens-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoadChildrensController } from '@/presentation/controllers'
 import { LoadChildrensSpy } from '@/tests/presentation/mocks'
 import { throwError } from '@/tests/domain/mocks'
-import { serverError } from '@/presentation/helpers'
+import { serverError, ok } from '@/presentation/helpers'
 import { ServerError } from '@/presentation/errors'
 
 import faker from 'faker'
@@ -37,5 +37,11 @@ describe('LoadChildrens Controller', () => {
     jest.spyOn(loadChildrensSpy, 'load').mockImplementationOnce(throwError)
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut, loadChildrensSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(loadChildrensSpy.result))
   })
 })
