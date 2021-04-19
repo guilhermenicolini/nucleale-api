c58a73e0c0d4dfba7bc27d89106c2dfefc07fcb5
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-19 19:19:37 -0300

    feat: ensure DeleteChildrenValidation calls ValidationComposite with all validations

--- src/main/factories/controllers/children/index.ts
+++ src/main/factories/controllers/children/index.ts
@@ -1,5 +1,6 @@
 export * from './add-children-controller-factory'
 export * from './add-children-validation-factory'
+export * from './delete-children-validation-factory'
 export * from './load-childrens-controller-factory'
 export * from './update-children-controller-factory'
 export * from './update-children-validation-factory'
