31c2c173b24c351ebaeda343909276ec87a4e549
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-19 07:57:36 -0300

    test: ensure DbUpdateChildren calls UpdateChildrenRepository with correct values

---
+++ tests/data/usecases/children/db-update-children.spec.ts
@@ -0,0 +1,27 @@
+import { DbUpdateChildren } from '@/data/usecases'
+import { UpdateChildrenRepositorySpy } from '@/tests/data/mocks'
+import { mockUpdateChildrenModel } from '@/tests/domain/mocks'
+
+type SutTypes = {
+  sut: DbUpdateChildren
+  updateChildrenRepositorySpy: UpdateChildrenRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const updateChildrenRepositorySpy = new UpdateChildrenRepositorySpy()
+  const sut = new DbUpdateChildren(updateChildrenRepositorySpy)
+
+  return {
+    sut,
+    updateChildrenRepositorySpy
+  }
+}
+
+describe('DbUpdateChildren Usecase', () => {
+  test('Should call UpdateChildrenRepository with correct values', async () => {
+    const { sut, updateChildrenRepositorySpy } = makeSut()
+    const params = mockUpdateChildrenModel()
+    await sut.update(params)
+    expect(updateChildrenRepositorySpy.params).toEqual(params)
+  })
+})
