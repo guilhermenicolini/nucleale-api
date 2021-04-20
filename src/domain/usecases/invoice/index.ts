fc5898d70b055df8b8c98cc7dd22fabc6de1af24
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-20 16:55:56 -0300

    feat: ensure ImportInvoicesController calls SaveInvoice with correct values

--- src/domain/usecases/invoice/index.ts
+++ src/domain/usecases/invoice/index.ts
@@ -1,1 +1,2 @@
 export * from './load-invoices'
+export * from './save-invoice'
--- src/presentation/controllers/invoice/import-invoices-controller.ts
+++ src/presentation/controllers/invoice/import-invoices-controller.ts
@@ -1,15 +1,19 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
-import { LoadInvoices } from '@/domain/usecases'
+import { LoadInvoices, SaveInvoice } from '@/domain/usecases'
 import { serverError, noContent } from '@/presentation/helpers'
 
 export class ImportInvoicesController implements Controller {
   constructor (
-    private readonly loadInvoices: LoadInvoices
+    private readonly loadInvoices: LoadInvoices,
+    private readonly saveInvoice: SaveInvoice
   ) { }
 
   async handle (): Promise<HttpResponse> {
     try {
-      await this.loadInvoices.load()
+      const invoices = await this.loadInvoices.load()
+      for (const invoice of invoices) {
+        await this.saveInvoice.save(invoice)
+      }
       return noContent()
     } catch (error) {
       return serverError(error)
