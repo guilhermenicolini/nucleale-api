e1c1e5ba43c1531b381aeb3d2b83a07aec8afef4
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-22 17:14:59 -0300

    feat: ensure File Parser parse file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { bodyParser, contentType, cors } from '@/main/middlewares'
+import { bodyParser, contentType, cors, fileParser } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export default (app: Express): void => {
+  app.use(fileParser)
   app.use(bodyParser)
   app.use(contentType)
   app.use(cors)
