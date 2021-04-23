7fe8cae22ea2e6cb74d4b9794b3a18ce5ef31305
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-23 12:49:00 -0300

    test: ensure DbSaveInvoice throws if SaveInvoiceRepository throws

--- tests/data/usecases/invoice/db-save-invoice.spec.ts
+++ tests/data/usecases/invoice/db-save-invoice.spec.ts
@@ -1,6 +1,6 @@
 import { DbSaveInvoice } from '@/data/usecases'
 import { SaveInvoiceRepositorySpy } from '@/tests/data/mocks'
-import { mockInvoice } from '@/tests/domain/mocks'
+import { mockInvoice, throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbSaveInvoice
@@ -23,5 +23,12 @@ describe('DbSaveInvoice Usecase', () => {
     const param = mockInvoice()
     await sut.save(param)
     expect(saveInvoiceRepositorySpy.param).toEqual(param)
+  })
+
+  test('Should throw if SaveInvoiceRepository throws', async () => {
+    const { sut, saveInvoiceRepositorySpy } = makeSut()
+    jest.spyOn(saveInvoiceRepositorySpy, 'save').mockImplementationOnce(throwError)
+    const promise = sut.save(mockInvoice())
+    await expect(promise).rejects.toThrow()
   })
 })
