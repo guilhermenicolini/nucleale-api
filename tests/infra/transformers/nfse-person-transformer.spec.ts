6f22e331b5162b217a4bd5840df628ad2ec4484e
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-24 13:42:21 -0300

    test: ensure NfsePersonTransformer returns invoice person on success

--- tests/infra/transformers/nfse-person-transformer.spec.ts
+++ tests/infra/transformers/nfse-person-transformer.spec.ts
@@ -2,21 +2,42 @@ import { NfsePersonTransformer } from '@/infra/transformers'
 import { mockNfseItem } from '@/tests/infra/mocks'
 
 const makeSutTaker = (): NfsePersonTransformer => new NfsePersonTransformer('taker', 'TOMADOR')
+const makeSutProvider = (): NfsePersonTransformer => new NfsePersonTransformer('provider', 'PRESTADOR', 'PREST')
 
 describe('NfsePerson Transformer', () => {
-  test('Should return invoice person model on success', () => {
-    const sut = makeSutTaker()
-    const data = mockNfseItem()
-    const result = sut.transform(data)
-    expect(result.taker).toBeTruthy()
+  describe('taker', () => {
+    test('Should return invoice person model on success', () => {
+      const sut = makeSutTaker()
+      const data = mockNfseItem()
+      const result = sut.transform(data)
+      expect(result.taker).toBeTruthy()
+    })
+
+    test('Should return invoice person model without optional props', () => {
+      const sut = makeSutTaker()
+      const data = mockNfseItem()
+      data.TOMADOR_TIPO_LOGRADOURO = null
+      data.TOMADOR_COMPLEMENTO = null
+      const result = sut.transform(data)
+      expect(result.taker).toBeTruthy()
+    })
   })
 
-  test('Should return invoice person model without optional props', () => {
-    const sut = makeSutTaker()
-    const data = mockNfseItem()
-    data.TOMADOR_TIPO_LOGRADOURO = null
-    data.TOMADOR_COMPLEMENTO = null
-    const result = sut.transform(data)
-    expect(result.taker).toBeTruthy()
+  describe('provider', () => {
+    test('Should return invoice person model on success', () => {
+      const sut = makeSutProvider()
+      const data = mockNfseItem()
+      const result = sut.transform(data)
+      expect(result.provider).toBeTruthy()
+    })
+
+    test('Should return invoice person model without optional props', () => {
+      const sut = makeSutProvider()
+      const data = mockNfseItem()
+      data.PRESTADOR_TIPO_LOGRADOURO = null
+      data.PRESTADOR_COMPLEMENTO = null
+      const result = sut.transform(data)
+      expect(result.provider).toBeTruthy()
+    })
   })
 })
