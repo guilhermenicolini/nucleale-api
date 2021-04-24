50ad6ddb7d5ece3e38577cb0fb24a38e2b6383ba
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-24 13:29:44 -0300

    refactor: validate only NfseTransformer object instance

--- tests/infra/transformers/nfse-transformer.spec.ts
+++ tests/infra/transformers/nfse-transformer.spec.ts
@@ -8,24 +8,6 @@ describe('Nfse Transformer', () => {
     const sut = makeSut()
     const data = mockNfseItem()
     const result = sut.transform(data)
-    expect(result).toEqual({
-      invoiceNo: result.invoiceNo,
-      invoiceDate: result.invoiceDate,
-      issueDate: result.issueDate,
-      verificationCode: result.verificationCode,
-      description: result.description,
-      invoiceValue: result.invoiceValue,
-      serviceValue: result.serviceValue,
-      issValue: result.issValue,
-      issAliquot: result.issAliquot,
-      competence: result.competence,
-      pickupType: result.pickupType,
-      taxation: result.taxation,
-      cnae: result.cnae,
-      activity: result.activity,
-      service: result.service,
-      serviceCity: result.serviceCity,
-      serviceState: result.serviceState
-    })
+    expect(result).toBeTruthy()
   })
 })
