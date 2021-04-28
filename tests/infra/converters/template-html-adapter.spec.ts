9499cdb481c9170d623080273fb90c30fadcd3eb
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-28 20:03:25 -0300

    test: ensure TemplateHtmlAdapters calls create html-pdf with correct values

--- tests/infra/converters/template-html-adapter.spec.ts
+++ tests/infra/converters/template-html-adapter.spec.ts
@@ -1,5 +1,6 @@
 import { TemplateHtmlAdapter } from '@/infra/converters'
 import Templates from 'email-templates'
+import pdf from 'html-pdf'
 
 const template = 'any_template'
 const makeSut = (): TemplateHtmlAdapter => new TemplateHtmlAdapter(template)
@@ -10,6 +11,14 @@ jest.mock('email-templates', () => jest.fn().mockImplementation(() => {
   return {
     render: renderStub
   }
+}))
+
+jest.mock('html-pdf', () => ({
+  create: jest.fn().mockImplementation(() => {
+    return {
+      toStream: jest.fn().mockImplementation(() => 'any_stream')
+    }
+  })
 }))
 
 describe('TemplateHtml Adapter', () => {
@@ -29,9 +38,19 @@ describe('TemplateHtml Adapter', () => {
     expect(renderStub).toHaveBeenCalledWith(template, 'any_message')
   })
 
-  test('Should return html on success', async () => {
+  test('Should create html-pdf with correct values', async () => {
+    const sut = makeSut()
+    await sut.convert('any_message')
+    expect(pdf.create).toHaveBeenCalledWith('any_html', {
+      height: '1123px',
+      width: '794px',
+      quality: '75'
+    })
+  })
+
+  test('Should return stream on success', async () => {
     const sut = makeSut()
     const result = await sut.convert('any_message')
-    expect(result).toBe('any_html')
+    expect(result).toBe('any_stream')
   })
 })
