c0ac003167fd74e10a14778c417a01c188e9dd61
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-28 14:04:51 -0300

    refactor: ensure NfseTransformer factory calls all transformers

--- src/main/factories/transformers/nfse-transformer-factory.ts
+++ src/main/factories/transformers/nfse-transformer-factory.ts
@@ -1,10 +1,11 @@
 import { TransformerComposite } from '@/data/composites'
 import { Transformer } from '@/data/protocols'
 import { NfseTransformer, NfsePersonTransformer, NfseItemsTransformer } from '@/infra/transformers'
+import { MomentAdapter } from '@/infra/manipulation'
 
 export const makeNfseTransformerComposite = (): TransformerComposite => {
   const transformers: Transformer[] = []
-  transformers.push(new NfseTransformer())
+  transformers.push(new NfseTransformer(new MomentAdapter()))
   transformers.push(new NfsePersonTransformer('taker', 'TOMADOR'))
   transformers.push(new NfsePersonTransformer('provider', 'PRESTADOR', 'PREST'))
   transformers.push(new NfseItemsTransformer())
--- tests/main/factories/transformers/nfse-transformer-factory.spec.ts
+++ tests/main/factories/transformers/nfse-transformer-factory.spec.ts
@@ -2,6 +2,7 @@ import { makeNfseTransformerComposite } from '@/main/factories'
 import { Transformer } from '@/data/protocols'
 import { TransformerComposite } from '@/data/composites'
 import { NfseTransformer, NfsePersonTransformer, NfseItemsTransformer } from '@/infra/transformers'
+import { MomentAdapter } from '@/infra/manipulation'
 
 jest.mock('@/data/composites/transformer-composite')
 
@@ -9,7 +10,7 @@ describe('NfseTransformerComposite Factory', () => {
   test('Should call TransformerComposite with all transformers', () => {
     makeNfseTransformerComposite()
     const transformers: Transformer[] = []
-    transformers.push(new NfseTransformer())
+    transformers.push(new NfseTransformer(new MomentAdapter()))
     transformers.push(new NfsePersonTransformer('taker', 'TOMADOR'))
     transformers.push(new NfsePersonTransformer('provider', 'PRESTADOR', 'PREST'))
     transformers.push(new NfseItemsTransformer())
