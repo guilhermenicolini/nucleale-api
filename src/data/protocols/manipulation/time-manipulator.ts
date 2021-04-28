b5f5ad06460f493196004b4ea1fc5ba0c5582d0d
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-28 14:01:37 -0300

    feat: ensure MoneyManipulator format on success

--- src/data/protocols/manipulation/time-manipulator.ts
+++ src/data/protocols/manipulation/time-manipulator.ts
@@ -2,4 +2,7 @@ export interface TimeManipulator {
   toDateAndTime: (millis: number) => string
   toDay: (millis: number) => string
   toDate: (millis: number) => string
+  toMonthAndYear: (millis: number) => string
+  fromDate: (value: string) => number
+  fromDateAndTime: (value: string) => number
 }
--- src/infra/manipulation/moment-adapter.ts
+++ src/infra/manipulation/moment-adapter.ts
@@ -3,21 +3,42 @@ import { TimeManipulator } from '@/data/protocols'
 import moment from 'moment-timezone'
 
 export class MomentAdapter implements TimeManipulator {
+  private readonly firstPart: string
+  private readonly secondPart: string
+  private readonly thirdPart: string
+  private readonly separator: string
+
   constructor (
-    private readonly dateAndTimeFormat: string,
-    private readonly dayFormat: string,
-    private readonly dateFormat: string
-  ) { }
+    dmy: 'dmy' | 'mdy' = 'dmy',
+    separator: string = '/'
+  ) {
+    this.firstPart = dmy === 'dmy' ? 'DD' : 'MM'
+    this.secondPart = dmy === 'dmy' ? 'MM' : 'DD'
+    this.thirdPart = 'YYYY'
+    this.separator = separator
+  }
 
   toDateAndTime (millis: number): string {
-    return moment(millis).format(this.dateAndTimeFormat)
+    return moment(millis).format(`${this.firstPart}${this.separator}${this.secondPart}${this.separator}${this.thirdPart} HH:mm`)
   }
 
   toDay (millis: number): string {
-    return moment(millis).format(this.dayFormat)
+    return moment(millis).format('DD')
   }
 
   toDate (millis: number): string {
-    return moment(millis).format(this.dateFormat)
+    return moment(millis).format(`${this.firstPart}${this.separator}${this.secondPart}${this.separator}${this.thirdPart}`)
+  }
+
+  toMonthAndYear (millis: number): string {
+    return moment(millis).format(`${this.separator}MM${this.separator}YYYY`)
+  }
+
+  fromDate (value: string): number {
+    return moment(value, `${this.firstPart}${this.separator}${this.secondPart}${this.separator}${this.thirdPart}`).valueOf()
+  }
+
+  fromDateAndTime (value: string): number {
+    return moment(value, `${this.firstPart}${this.separator}${this.secondPart}${this.separator}${this.thirdPart} HH:mm:ss`).valueOf()
   }
 }
