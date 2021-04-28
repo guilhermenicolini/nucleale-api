be884b5180917586f8004d1dfb558d5cb9634112
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-28 19:35:18 -0300

    test: ensure TemplateHtmlAdapter calls render with correct values

--- tests/infra/converters/template-html-adapter.spec.ts
+++ tests/infra/converters/template-html-adapter.spec.ts
@@ -4,7 +4,13 @@ import Templates from 'email-templates'
 const template = 'any_template'
 const makeSut = (): TemplateHtmlAdapter => new TemplateHtmlAdapter(template)
 
-jest.mock('email-templates')
+const renderStub = jest.fn().mockImplementation(() => 'any_html')
+
+jest.mock('email-templates', () => jest.fn().mockImplementation(() => {
+  return {
+    render: renderStub
+  }
+}))
 
 describe('TemplateHtml Adapter', () => {
   test('Should init email-templates with correct values', async () => {
@@ -15,5 +21,11 @@ describe('TemplateHtml Adapter', () => {
         root: 'src/templates'
       }
     })
+  })
+
+  test('Should call render correct values', async () => {
+    const sut = makeSut()
+    await sut.convert('any_message')
+    expect(renderStub).toHaveBeenCalledWith(template, 'any_message')
   })
 })
