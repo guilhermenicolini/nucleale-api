27b97fa5c95cefe860b5e30dc0baae5096595bc7
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-04-29 23:05:09 -0300

    test: test 100 percent invoice pdf message converter

--- tests/infra/converters/invoice-pdf-message-converter.spec.ts
+++ tests/infra/converters/invoice-pdf-message-converter.spec.ts
@@ -2,22 +2,41 @@ import { InvoicePdfMessageConverter } from '@/infra/converters'
 import { mockInvoiceDb } from '@/tests/domain/mocks'
 import { TimeManipulatorSpy, MoneyManipulatorSpy, MaskManipulatorSpy } from '@/tests/data/mocks'
 
-const makeSut = (): InvoicePdfMessageConverter => new InvoicePdfMessageConverter(
-  new TimeManipulatorSpy(),
-  new MoneyManipulatorSpy(),
-  new MaskManipulatorSpy()
-)
+type SutTypes = {
+  sut: InvoicePdfMessageConverter,
+  timeManipulatorSpy: TimeManipulatorSpy,
+  moneyManipulatorSpy: MoneyManipulatorSpy,
+  maskManipulatorSpy: MaskManipulatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const timeManipulatorSpy = new TimeManipulatorSpy()
+  const moneyManipulatorSpy = new MoneyManipulatorSpy()
+  const maskManipulatorSpy = new MaskManipulatorSpy()
+  const sut = new InvoicePdfMessageConverter(
+    timeManipulatorSpy,
+    moneyManipulatorSpy,
+    maskManipulatorSpy
+  )
+  return {
+    sut,
+    timeManipulatorSpy,
+    moneyManipulatorSpy,
+    maskManipulatorSpy
+  }
+}
 
 describe('InvoicePdfMessage Converter', () => {
   test('Should return message on success', () => {
-    const sut = makeSut()
+    const { sut, timeManipulatorSpy } = makeSut()
     const data = mockInvoiceDb()
+    jest.spyOn(timeManipulatorSpy, 'toDay').mockImplementationOnce(() => 'any_date2')
     const result = sut.convert(data)
     expect(result).toBeTruthy()
   })
 
   test('Should return message alternative values on success', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const data = mockInvoiceDb()
     data.items[0].taxable = true
     data.items[1].taxable = false
