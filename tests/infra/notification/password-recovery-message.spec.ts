fa768c5f603fb3d87b74ddc880d347378de053f1
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-05-01 20:20:59 -0300

    test: ensure PasswordRecoveryMessage create message

---
+++ tests/infra/notification/password-recovery-message.spec.ts
@@ -0,0 +1,25 @@
+import { PasswordRecoveryMessage } from '@/infra/notification'
+
+import faker from 'faker'
+
+const makeSut = (): PasswordRecoveryMessage => new PasswordRecoveryMessage()
+
+describe('PasswordRecoveryMessage', () => {
+  test('Should create message on success', () => {
+    const sut = makeSut()
+    const model: PasswordRecoveryMessage.Model = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+      link: faker.internet.url(),
+      expiration: faker.random.word()
+    }
+    const message = sut.create(model)
+    expect(message).toEqual({
+      email: model.email,
+      phone: model.phone,
+      text: `Olá, ${model.name}. Acesse ${model.link} para redefinir sua senha. Este link será válido até às ${model.expiration}`,
+      html: null
+    })
+  })
+})
