a74bfe05fec0cb0bbe8f6c8337a1b76b4de2f198
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-05-01 09:13:36 -0300

    chore: remove SendMessage usecase

--- src/domain/usecases/notification/send-message.ts
+++
@@ -1,9 +0,0 @@
-import { MessageModel } from '@/domain/models'
-
-export interface SendMessage {
-  send: (message: SendMessage.Params) => Promise<void>
-}
-
-export namespace SendMessage {
-  export type Params = MessageModel
-}
