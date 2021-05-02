cc0c6c8b908a55e624e3a6cc09357768b56ca20c
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-05-02 13:52:27 -0300

    refactor: mock sendMessage return

--- src/main/factories/controllers/authentication/index.ts
+++ src/main/factories/controllers/authentication/index.ts
@@ -1,5 +1,6 @@
 export * from './login-controller-factory'
 export * from './login-validation-factory'
+export * from './password-recovery-controller-factory'
 export * from './password-recovery-validation-factory'
 export * from './signup-controller-factory'
 export * from './signup-validation-factory'
--- tests/mock.ts
+++ tests/mock.ts
@@ -1,4 +1,4 @@
-export const sendMessageStub = jest.fn()
+export const sendMessageStub = jest.fn().mockImplementation((phone, text) => console.log(phone, text))
 
 jest.mock('@wppconnect-team/wppconnect', jest.fn().mockImplementation(() => {
   return {
