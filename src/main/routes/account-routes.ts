e926b1b00bdfa60991ffba0325595ef76f23b70e
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-05-02 13:39:18 -0300

    refactor: separate uthentication routes

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,7 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
 import {
-  makeSignUpController,
-  makeLoginController,
   makeLoadAccountsByStatusController,
   makeApproveAccountController,
   makeInviteAccountController
@@ -11,9 +9,6 @@ import { adminAuth, auth } from '@/main/middlewares'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-  router.post('/login', adaptRoute(makeLoginController()))
-
   router.get('/accounts/status/:status', adminAuth, adaptRoute(makeLoadAccountsByStatusController()))
   router.post('/accounts/:id/approve', adminAuth, adaptRoute(makeApproveAccountController()))
   router.post('/accounts/invite/:email', auth, adaptRoute(makeInviteAccountController()))
