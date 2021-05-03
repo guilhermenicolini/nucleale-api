f8d6142d2bebc7d7955ee9f7ae08278b59dced88
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-05-03 12:30:46 -0300

    test: ensure GoogleTokenStoreAdapter returns null if token not exists

--- tests/infra/notification/google-token-store-adapter.spec.ts
+++ tests/infra/notification/google-token-store-adapter.spec.ts
@@ -4,10 +4,32 @@ import faker from 'faker'
 const sessionName = faker.random.word().toLowerCase()
 const makeSut = (): GoogleTokenStore => new GoogleTokenStore()
 
+let existsStub = jest.fn().mockImplementation(() => [true])
+
+jest.mock('@google-cloud/storage', () => ({
+  Storage: jest.fn().mockImplementation(() => {
+    return {
+      bucket: jest.fn().mockImplementation(() => ({
+        file: jest.fn().mockImplementation(() => ({
+          exists: existsStub,
+          download: jest.fn().mockImplementation(() => Buffer.from(JSON.stringify({ ok: 'ok' }), 'utf8'))
+        }))
+      }))
+    }
+  })
+}))
+
 describe('GoogleTokenStore Adapter', () => {
   test('Should get token on success', async () => {
     const sut = makeSut()
     const token = await sut.getToken(sessionName)
     expect(token).toEqual({ ok: 'ok' })
+  })
+
+  test('Should return null if token not exists', async () => {
+    const sut = makeSut()
+    existsStub = jest.fn().mockImplementation(() => [false])
+    const token = await sut.getToken(sessionName)
+    expect(token).toBeFalsy()
   })
 })
