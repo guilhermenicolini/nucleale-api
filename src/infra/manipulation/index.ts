21233ad5400de17671f45bb757ced19799c60914
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-06-08 09:52:25 -0300

    feat: ensure StringFormatAdapter manipulate string correctly

--- src/infra/manipulation/index.ts
+++ src/infra/manipulation/index.ts
@@ -1,3 +1,4 @@
 export * from './moment-adapter'
 export * from './money-adapter'
+export * from './string-format-adapter'
 export * from './string-mask-adapter'
---
+++ src/infra/manipulation/string-format-adapter.ts
@@ -0,0 +1,38 @@
+import { StringManipulator } from '@/data/protocols'
+
+export class StringFormatAdapter implements StringManipulator {
+  format (value: string, args: any | any[] = []): string {
+    if (!value) return ''
+    if (!Array.isArray(args)) args = [args]
+
+    for (let i = 0; i < args.length; i++) {
+      const mask = '{' + i + '}'
+      value = value.replace(mask, args[i])
+    }
+
+    return value
+  }
+
+  normalize (value: string): string {
+    if (!value) return ''
+    value = value.trim()
+    return value
+      .split('')
+      .map(
+        function (letter) {
+          const i = this.accents.indexOf(letter)
+          return i !== -1 ? this.out[i] : letter
+        }.bind({
+          accents:
+            'ÀÁÂÃÄÅĄĀāàáâãäåąßÒÓÔÕÕÖØŐòóôőõöøĎďDŽdžÈÉÊËĘèéêëęðÇçČčĆćÐÌÍÎÏĪìíîïīÙÚÛÜŰùűúûüĽĹŁľĺłÑŇŃňñńŔŕŠŚŞšśşŤťŸÝÿýŽŻŹžżźđĢĞģğ',
+          out:
+            'AAAAAAAAaaaaaaaasOOOOOOOOoooooooDdDZdzEEEEEeeeeeeCcCcCcDIIIIIiiiiiUUUUUuuuuuLLLlllNNNnnnRrSSSsssTtYYyyZZZzzzdGGgg'
+        })
+      )
+      .join('')
+      .replace(/&AMP;LT;BR/g, '')
+      .replace(/\/&AMP;GT;/g, '')
+      .replace('  ', ' ')
+      .toUpperCase()
+  }
+}
