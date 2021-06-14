6e7ac988e5a7d8cb6a68f10f5dc67910364f2fa3
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-06-14 12:16:14 -0300

    feat: ensure CreateInvoiceValidation calls ValidationComposite with all validations

--- src/main/factories/controllers/invoice/index.ts
+++ src/main/factories/controllers/invoice/index.ts
@@ -1,3 +1,4 @@
+export * from './create-invoice-validation-factory'
 export * from './download-invoice-controller-factory'
 export * from './download-invoice-validation-factory'
 export * from './load-invoices-controller-factory'
