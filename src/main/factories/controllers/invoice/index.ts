90930506b13295de1ae17d1096c21a5059bacf8c
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-06-14 17:18:36 -0300

    feat: ensure POST /invoices returns 401 on invalid token

--- src/main/factories/controllers/invoice/index.ts
+++ src/main/factories/controllers/invoice/index.ts
@@ -1,3 +1,4 @@
+export * from './create-invoice-controller-factory'
 export * from './create-invoice-validation-factory'
 export * from './download-invoice-controller-factory'
 export * from './download-invoice-validation-factory'
