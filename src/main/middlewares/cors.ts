6748587ad11a2417a44a7cec77cfba6afd4cf471
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-10-07 14:43:24 -0300

    feat: expose content-disposition header

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -4,5 +4,6 @@ export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('access-control-allow-origin', '*')
   res.set('access-control-allow-headers', '*')
   res.set('access-control-allow-methods', '*')
+  res.set('access-control-expose-headers', 'Content-Disposition')
   next()
 }
